9cb42bbfbf5634e842c2dd64eb07413ac069e97d
Gauraang P. Kelkar <gauraangkelkar@gmail.com>
2020-10-18 16:40:41 -0400

    Change data type of sum from int to long

--- SumofSeries/Program.cs
+++ SumofSeries/Program.cs
@@ -11,21 +11,18 @@ namespace SumofSeries
             string[] numbers = inputExpression.Split('+');
             if (numbers.Length != 0)
             {
-                int sum = 0;
+                long sum = 0;
                 foreach(var elem in numbers)
                 {
                     bool result = int.TryParse(elem, out int number);
                     if (result)
-                    {
-                        sum += number;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Incorrect input value {0}. Please enter a valid expression.", elem);
+                            sum += number;
+                    else {
+                        Console.WriteLine("Incorrect input value {0}, please enter a valid expression.", elem);
                         return;
                     }
                 }
-                Console.WriteLine("The sum of series is:{0}", sum);
+                Console.WriteLine("The sum of series  is: {0}", sum);
             }
             else
             {
